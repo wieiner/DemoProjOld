57107cbddbb5b5a796c312afc7e933fbfa6a4aba
Aleksander Militsin <hal900151@gmail.com>
2023-07-13 11:45:46 +0300

    new converter added

---
+++ src/main/java/com/demo/demoproj/service/converter/UserItemDTOConverter.java
@@ -0,0 +1,17 @@
+package com.demo.demoproj.service.converter;
+
+import com.demo.demoproj.repository.model.UserItem;
+import com.demo.demoproj.service.model.UserItemDTO;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserItemDTOConverter implements Converter<UserItemDTO, UserItem> {
+
+    @Override
+    public UserItem convert(@NotNull UserItemDTO from) {
+        return new UserItem(from.getId(), from.getUsername(), from.getPassword());
+    }
+
+}
